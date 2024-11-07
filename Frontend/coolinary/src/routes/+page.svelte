07c5d72384878b743ae679ae4922e7b7d5359b10
Alastair7 <alexandru.ban.drmer@gmail.com>
2024-11-07 23:42:10 +0100

    add playwright and first test for homepage

--- Frontend/coolinary/src/routes/+page.svelte
+++ Frontend/coolinary/src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<header>
+    <h1>COOLINARY</h1>
+    <h2>Welcome to Coolinary a place where you can share your receipts</h2>
+</header>
+
+<style>
+    header {
+        display:flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
