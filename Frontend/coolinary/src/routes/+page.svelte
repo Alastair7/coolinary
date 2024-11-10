e063a12b75b2854b5a8fc8a47a3ac1e336d1a2df
Alastair7 <alexandru.ban.drmer@gmail.com>
2024-11-10 13:42:31 +0100

    add login button

--- Frontend/coolinary/src/routes/+page.svelte
+++ Frontend/coolinary/src/routes/+page.svelte
@@ -2,6 +2,9 @@
     <h1>COOLINARY</h1>
     <h2>Welcome to Coolinary a place where you can share your recipees</h2>
 </header>
+<div class="buttons-wrapper">
+    <button>Login</button>
+</div>
 
 <style>
     header {
@@ -9,5 +12,18 @@
         flex-direction: column;
         justify-content: center;
         align-items: center;
+    }
+
+    .buttons-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 2rem;
+    }
+    h1 {
+        color: #458f45;
+    }
+    h2 {
+        color: #224822
     }
 </style>
--- Frontend/coolinary/tests/pages/homepage.spec.ts
+++ Frontend/coolinary/tests/pages/homepage.spec.ts
@@ -1,18 +1,28 @@
-import { expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 import { render, screen } from '@testing-library/svelte';
 
 import Page from '../../src/routes/+page.svelte';
 
-test('has headers', async () => {
-	render(Page);
+describe('Homepage', async () => {
+	it('has headers when rendered', () => {
+		render(Page);
 
-	const expectedTitle = 'COOLINARY';
-	const expectedSubtitle = 'Welcome to Coolinary a place where you can share your recipees';
+		const expectedTitle = 'COOLINARY';
+		const expectedSubtitle = 'Welcome to Coolinary a place where you can share your recipees';
 
-	const titleHeader = screen.getByRole('heading', { level: 1 });
-	const subtitleHeader = screen.getByRole('heading', { level: 2 });
+		const titleHeader = screen.getByRole('heading', { level: 1 });
+		const subtitleHeader = screen.getByRole('heading', { level: 2 });
 
-	expect(titleHeader).toHaveTextContent(expectedTitle);
-	expect(subtitleHeader).toHaveTextContent(expectedSubtitle);
+		expect(titleHeader).toHaveTextContent(expectedTitle);
+		expect(subtitleHeader).toHaveTextContent(expectedSubtitle);
+	});
+
+	it('has login button when rendered', () => {
+		render(Page);
+
+		const loginButton = screen.getByRole('button', { name: 'Login' });
+
+		expect(loginButton).toBeVisible();
+	});
 });
